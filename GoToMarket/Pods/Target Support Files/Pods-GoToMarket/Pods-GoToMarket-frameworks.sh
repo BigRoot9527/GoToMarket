40934662573516008240a23533ea4624e5102b9f
許庭瑋 <jeremiah79324@gmail.com>
2018-05-11 12:19:02 +0800

    pods installed, try to fix git problem

--- GoToMarket/Pods/Target Support Files/Pods-GoToMarket/Pods-GoToMarket-frameworks.sh
+++ GoToMarket/Pods/Target Support Files/Pods-GoToMarket/Pods-GoToMarket-frameworks.sh
@@ -144,9 +144,15 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/BusyNavigationBar/BusyNavigationBar.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TransitionButton/TransitionButton.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/BusyNavigationBar/BusyNavigationBar.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TransitionButton/TransitionButton.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
