6d03a5419c95d32847a67bd81f3e7e89ee9a55d5
許庭瑋 <jeremiah79324@gmail.com>
2018-05-23 13:51:53 +0800

    prepare to build note VC another way

---
+++ GoToMarket/GoToMarket/Controller/Note/NoteViewController.swift
@@ -0,0 +1,242 @@
+//
+//  NoteViewController.swift
+//  GoToMarket
+//
+//  Created by 許庭瑋 on 2018/5/23.
+//  Copyright © 2018年 許庭瑋. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+private enum noteCellStatus {
+    
+    case open
+    
+    case close
+    
+    func height() -> CGFloat {
+        switch self {
+        case .open:
+            return 200.0
+        case .close:
+            return 100.0
+        }
+    }
+}
+
+
+class NoteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
+    
+    @IBOutlet weak var noteTableView: UITableView!
+    
+    private var noteCellState: [noteCellStatus]? {
+        didSet {
+            print("been set!")
+        }
+    }
+    
+    var container: NSPersistentContainer? =
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer { didSet { fetchAndReloadData() } }
+    var fetchedResultsController: NSFetchedResultsController<UserNotes>?
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = fetchedResultsController?.sections, sections.count > 0 {
+            
+            let count = sections[section].numberOfObjects
+            
+            if noteCellState == nil {
+                
+                noteCellState = (0 ..< count).map{ _ in .close }
+                
+            }
+            
+            print("count = \(count)")
+            
+            return count
+            
+        } else {
+            
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: NoteTableViewCell.self), for: indexPath) as! NoteTableViewCell
+        
+        setupCell(atIndexpath: indexPath, cellBeforeInit: cell)
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        guard let state = noteCellState else { return 10.0 }
+        
+        return state[indexPath.row].height()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let cell = self.noteTableView.cellForRow(at: indexPath) as? NoteTableViewCell, var state = noteCellState else { return }
+        
+        if cell.isAnimating() {
+            return
+        }
+        
+        var duration = 0.0
+        
+        if state[indexPath.row] == .close { // open cell
+            
+            state[indexPath.row] = .open
+            
+            self.noteCellState = state
+            
+            setupCell(atIndexpath: indexPath, cellBeforeInit: nil)
+            
+            cell.openAnimation {}
+            
+            duration = 0.3
+            
+        } else {// close cell
+            
+            state[indexPath.row] = .close
+            
+            self.noteCellState = state
+            
+            setupCell(atIndexpath: indexPath, cellBeforeInit: nil)
+            
+            cell.closeAnimation {}
+            
+            duration = 0.3
+        }
+        
+        print("didSelect indexpath = \(indexPath)")
+        
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            options: .curveEaseIn, animations: { [weak self] in
+
+                self?.noteTableView.beginUpdates()
+                self?.noteTableView.endUpdates()
+
+                
+        }, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        guard
+            let showingCell = cell as? NoteTableViewCell,
+            let state = noteCellState
+            else { return }
+        
+        switch state[indexPath.row] {
+            
+        case .close:
+            //            break
+            showingCell.unfold(false, animated: false, completion: nil)
+            
+        case .open:
+            //            showingCell.openAnimation{}
+            showingCell.unfold(true, animated: false, completion: nil)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        noteTableView.delegate = self
+        noteTableView.dataSource = self
+        registerCell()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        fetchAndReloadData()
+    }
+
+    private func fetchAndReloadData() {
+        
+        if let context = container?.viewContext {
+            
+            let request: NSFetchRequest<UserNotes> = UserNotes.fetchRequest()
+            
+            request.sortDescriptors = [NSSortDescriptor(key: "isFinished", ascending: true)]
+            
+            request.predicate = NSPredicate(format: "isInCart = true")
+            
+            fetchedResultsController = NSFetchedResultsController<UserNotes>(
+                fetchRequest: request,
+                managedObjectContext: context,
+                sectionNameKeyPath: nil,
+                cacheName: nil
+            )
+            
+            fetchedResultsController?.delegate = self
+            
+            try? fetchedResultsController?.performFetch()
+            
+            noteTableView.reloadData()
+        }
+    }
+    
+    private func setupCell(atIndexpath indexPath: IndexPath, cellBeforeInit newCell: NoteTableViewCell?) {
+        
+        guard
+            let state = noteCellState,
+            let note = fetchedResultsController?.object(at: indexPath),
+            let cell = newCell ?? self.noteTableView.cellForRow(at: indexPath) as? NoteTableViewCell
+            else { return }
+
+        switch state[indexPath.row] {
+            
+        case .close:
+            
+            print("row: \(indexPath.row) = close")
+            cell.topBuyingAmountLabel.text = String(note.buyingAmount)
+            cell.topFinishButton.isSelected = note.isFinished
+            //TODO
+            cell.topWeightTypeLabel.text = "(每公斤)"
+            
+            if let cropData = note.cropData {
+                
+                cell.topCellPriceLabel.text = String(cropData.newAveragePrice * note.customMutipler)
+                cell.topItemNameLabel.text = cropData.cropName
+            }
+            
+            
+        case .open:
+            
+            cell.bottomFinishButton.isSelected = note.isFinished
+            //TODO
+            cell.bottomWeightTypeLabel.text = "(每公斤)"
+            cell.bottomBuyingAmountTextField.text = String(note.buyingAmount)
+            
+            if let cropData = note.cropData {
+                
+                cell.bottomItemNameLabel.text = cropData.cropName
+                cell.bottomSellPriceLabel.text = String(cropData.newAveragePrice * note.customMutipler)
+                cell.bottomNewRealPriceLabel.text = String(cropData.newAveragePrice)
+                cell.bottomLastRealPriceLabel.text = String(cropData.lastAveragePrice)
+            }
+        }
+    }
+    
+    private func registerCell() {
+        
+        let nibContent = UINib(nibName: "NoteTableViewCell", bundle: nil)
+        
+        noteTableView.register(
+            nibContent,
+            forCellReuseIdentifier: String(describing: NoteTableViewCell.self)
+        )
+    }
+}
---
+++ GoToMarket/GoToMarket/View/NoteTableViewCell/NoteTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  NoteTableViewCell.swift
+//  GoToMarket
+//
+//  Created by 許庭瑋 on 2018/5/23.
+//  Copyright © 2018年 許庭瑋. All rights reserved.
+//
+
+import UIKit
+import FoldingCell
+
+class NoteTableViewCell: FoldingCell {
+    
+    
+    //MARK: FoldingCell
+    @IBOutlet weak var topCellView: RotatedView!
+    @IBOutlet weak var topCellViewToContentViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomCellView: UIView!
+    @IBOutlet weak var bottomCellViewToContentViewTopConstraint: NSLayoutConstraint!
+    
+    //MARK: TopViewOutlets
+    @IBOutlet weak var topFinishButton: UIButton!
+    @IBOutlet weak var topItemNameLabel: UILabel!
+    @IBOutlet weak var topCellPriceLabel: UILabel!
+    @IBOutlet weak var topBuyingAmountLabel: UILabel!
+    @IBOutlet weak var topWeightTypeLabel: UILabel!
+    
+    //MARK: BottomViewOutlets
+    @IBOutlet weak var bottomFinishButton: UIButton!
+    @IBOutlet weak var bottomItemNameLabel: UILabel!
+    @IBOutlet weak var bottomDeleteButton: UIButton!
+    @IBOutlet weak var bottomSellPriceLabel: UILabel!
+    @IBOutlet weak var bottomNewRealPriceLabel: UILabel!
+    @IBOutlet weak var bottomLastRealPriceLabel: UILabel!
+    @IBOutlet weak var bottomPriceInfoButton: UIButton!
+    @IBOutlet weak var bottomWeightTypeLabel: UILabel!
+    @IBOutlet weak var bottomBuyingAmountTextField: UITextField!
+    @IBOutlet weak var bottomBuyingAmountStepper: UIStepper!
+    
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+////
+//        super.containerView = bottomCellView
+//        super.foregroundView = topCellView
+//        super.foregroundViewTop = topCellViewToContentViewTopConstraint
+//        super.containerViewTop = bottomCellViewToContentViewTopConstraint
+//        containerView = bottomCellView
+//        foregroundView = topCellView
+//        foregroundViewTop = topCellViewToContentViewTopConstraint
+//        containerViewTop = bottomCellViewToContentViewTopConstraint
+////
+//////        fatalError("init(coder:) has not been implemented")
+//        
+//    }
+//    
+    override func awakeFromNib() {
+        
+        self.containerView = bottomCellView
+        self.foregroundView = topCellView
+        self.foregroundViewTop = topCellViewToContentViewTopConstraint
+        self.containerViewTop = bottomCellViewToContentViewTopConstraint
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func animationDuration(_ itemIndex:NSInteger, type:AnimationType)-> TimeInterval {
+        
+        // durations count equal it itemCount
+        let durations = [0.25, 0.25] // timing animation for each view
+        return durations[itemIndex]
+    }
+
+}
