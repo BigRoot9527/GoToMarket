a1dac3ec56f0663b0d5b0766aa3f07c529257dc5
許庭瑋 <jeremiah79324@gmail.com>
2018-05-23 15:21:35 +0800

    try to allow only one cell open

--- GoToMarket/GoToMarket/Controller/Note/NoteViewController.swift
+++ GoToMarket/GoToMarket/Controller/Note/NoteViewController.swift
@@ -18,9 +18,9 @@ private enum noteCellStatus {
     func height() -> CGFloat {
         switch self {
         case .open:
-            return 200.0
+            return 210.0
         case .close:
-            return 100.0
+            return 110.0
         }
     }
 }
@@ -40,119 +40,13 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
         (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer { didSet { fetchAndReloadData() } }
     var fetchedResultsController: NSFetchedResultsController<UserNotes>?
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let sections = fetchedResultsController?.sections, sections.count > 0 {
-            
-            let count = sections[section].numberOfObjects
-            
-            if noteCellState == nil {
-                
-                noteCellState = (0 ..< count).map{ _ in .close }
-                
-            }
-            
-            print("count = \(count)")
-            
-            return count
-            
-        } else {
-            
-            return 0
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: NoteTableViewCell.self), for: indexPath) as! NoteTableViewCell
-        
-        setupCell(atIndexpath: indexPath, cellBeforeInit: cell)
-        
-        return cell
-    }
-    
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-        guard let state = noteCellState else { return 10.0 }
-        
-        return state[indexPath.row].height()
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        guard let cell = self.noteTableView.cellForRow(at: indexPath) as? NoteTableViewCell, var state = noteCellState else { return }
-        
-        if cell.isAnimating() {
-            return
-        }
-        
-        var duration = 0.0
-        
-        if state[indexPath.row] == .close { // open cell
-            
-            state[indexPath.row] = .open
-            
-            self.noteCellState = state
-            
-            setupCell(atIndexpath: indexPath, cellBeforeInit: nil)
-            
-            cell.openAnimation {}
-            
-            duration = 0.3
-            
-        } else {// close cell
-            
-            state[indexPath.row] = .close
-            
-            self.noteCellState = state
-            
-            setupCell(atIndexpath: indexPath, cellBeforeInit: nil)
-            
-            cell.closeAnimation {}
-            
-            duration = 0.3
-        }
-        
-        print("didSelect indexpath = \(indexPath)")
-        
-        UIView.animate(
-            withDuration: duration,
-            delay: 0,
-            options: .curveEaseIn, animations: { [weak self] in
-
-                self?.noteTableView.beginUpdates()
-                self?.noteTableView.endUpdates()
-
-                
-        }, completion: nil)
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        
-        guard
-            let showingCell = cell as? NoteTableViewCell,
-            let state = noteCellState
-            else { return }
-        
-        switch state[indexPath.row] {
-            
-        case .close:
-            //            break
-            showingCell.unfold(false, animated: false, completion: nil)
-            
-        case .open:
-            //            showingCell.openAnimation{}
-            showingCell.unfold(true, animated: false, completion: nil)
-        }
-    }
-    
-    
+    //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         noteTableView.delegate = self
         noteTableView.dataSource = self
+        noteTableView.estimatedRowHeight = noteCellStatus.close.height()
         registerCell()
     }
     
@@ -163,6 +57,7 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
         fetchAndReloadData()
     }
 
+    //MARK: CoreData
     private func fetchAndReloadData() {
         
         if let context = container?.viewContext {
@@ -187,6 +82,147 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
             noteTableView.reloadData()
         }
     }
+
+    
+    private func registerCell() {
+        
+        let nibContent = UINib(nibName: "NoteTableViewCell", bundle: nil)
+        
+        noteTableView.register(
+            nibContent,
+            forCellReuseIdentifier: String(describing: NoteTableViewCell.self)
+        )
+    }
+    
+    //MARK: TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = fetchedResultsController?.sections, sections.count > 0 {
+            
+            let count = sections[section].numberOfObjects
+            
+            if noteCellState == nil {
+                
+                noteCellState = (0 ..< count).map{ _ in .close }
+                
+            }
+            
+            print("count = \(count)")
+            
+            return count
+            
+        } else {
+            
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: NoteTableViewCell.self),
+            for: indexPath) as! NoteTableViewCell
+        
+        setupCell(atIndexpath: indexPath, cellBeforeInit: cell)
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        guard let state = noteCellState else {
+            
+            return 10.0
+        }
+        
+        print(indexPath.row)
+        
+        return state[indexPath.row].height()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard
+            let cell = self.noteTableView.cellForRow(at: indexPath) as? NoteTableViewCell,
+            var state = noteCellState else { return }
+        
+        if cell.isAnimating() {
+            
+            return
+        }
+        
+        var duration = 0.0
+        
+        if state[indexPath.row] == .close { // open cell
+            
+            state[indexPath.row] = .open
+            
+            self.noteCellState = state
+            
+            setupCell(atIndexpath: indexPath, cellBeforeInit: nil)
+            
+            cell.unfold(true, animated: true, completion: nil)
+            
+            duration = 0.3
+            
+            UIView.animate(
+                withDuration: duration,
+                delay: 0,
+                animations: {
+                    
+                    tableView.beginUpdates()
+                    tableView.endUpdates()
+                    
+            }, completion: nil)
+            
+        } else {// close cell
+            
+            state[indexPath.row] = .close
+            
+            self.noteCellState = state
+            
+            setupCell(atIndexpath: indexPath, cellBeforeInit: nil)
+            
+            cell.unfold(false, animated: true, completion: nil)
+            
+            duration = 0.3
+            
+            UIView.animate(
+                withDuration: duration,
+                delay: 0,
+                animations: {
+                    
+                    tableView.beginUpdates()
+                    tableView.endUpdates()
+                    
+            }, completion: nil)
+        }
+        
+        print("didSelect indexpath = \(indexPath)")
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        guard
+            let showingCell = cell as? NoteTableViewCell,
+            let state = noteCellState
+            else { return }
+
+        switch state[indexPath.row] {
+
+        case .close:
+            //                        break
+            showingCell.unfold(false, animated: false, completion: nil)
+
+        case .open:
+            //            showingCell.openAnimation{}
+            showingCell.unfold(true, animated: false, completion: nil)
+        }
+    }
+    
     
     private func setupCell(atIndexpath indexPath: IndexPath, cellBeforeInit newCell: NoteTableViewCell?) {
         
@@ -205,6 +241,7 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
             cell.topFinishButton.isSelected = note.isFinished
             //TODO
             cell.topWeightTypeLabel.text = "(每公斤)"
+//            cell.unfold(false, animated: false, completion: nil)
             
             if let cropData = note.cropData {
                 
@@ -219,6 +256,7 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
             //TODO
             cell.bottomWeightTypeLabel.text = "(每公斤)"
             cell.bottomBuyingAmountTextField.text = String(note.buyingAmount)
+//            cell.unfold(true, animated: false, completion: nil)
             
             if let cropData = note.cropData {
                 
@@ -228,15 +266,5 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
                 cell.bottomLastRealPriceLabel.text = String(cropData.lastAveragePrice)
             }
         }
-    }
-    
-    private func registerCell() {
-        
-        let nibContent = UINib(nibName: "NoteTableViewCell", bundle: nil)
-        
-        noteTableView.register(
-            nibContent,
-            forCellReuseIdentifier: String(describing: NoteTableViewCell.self)
-        )
     }
 }
