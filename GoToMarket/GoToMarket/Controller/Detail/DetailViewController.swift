8ca77d4418ce411a95098bbc10d7400d91dbc2a9
許庭瑋 <jeremiah79324@gmail.com>
2018-06-02 17:04:00 +0800

    fixed the detailVC status bug:  implement setting in info plist to make status bar hidden state works

--- GoToMarket/GoToMarket/Controller/Detail/DetailViewController.swift
+++ GoToMarket/GoToMarket/Controller/Detail/DetailViewController.swift
@@ -47,11 +47,6 @@ class DetailViewController: UIViewController {
     private var incartMessageText: String = ""
     private var incartImage: UIImage = UIImage()
     
-//    //MARK: - hide Status Bar
-//    override var prefersStatusBarHidden: Bool {
-//        return true
-//    }
-    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
