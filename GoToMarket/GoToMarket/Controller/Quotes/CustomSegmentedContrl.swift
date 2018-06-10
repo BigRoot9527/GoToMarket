351083a74fc6a7a87d8b4b7a8278fc58a2627111
許庭瑋 <jeremiah79324@gmail.com>
2018-06-10 19:14:19 +0800

    delete under line in custom segment

--- GoToMarket/GoToMarket/Controller/Quotes/CustomSegmentedContrl.swift
+++ GoToMarket/GoToMarket/Controller/Quotes/CustomSegmentedContrl.swift
@@ -26,6 +26,7 @@ import UIKit
             updateSegmentedControlSegs(index: selectedSegmentIndex)
         }
     }
+    
     var numberOfSegments: Int = 0 {
         didSet {
             numberOfSegments = buttons.count
@@ -99,23 +100,12 @@ import UIKit
             button.setTitleColor(textColor, for: .normal)
             button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
             buttons.append(button)
-            // button.setTitleColor(button.isSelected ? UIColor.gray : selectorTextColor, for: .normal)
         }
+        
         numberOfSegments = buttons.count
         buttons[0].setTitleColor(selectorTextColor, for: .normal)
-        // For UnderLiner For SegmentedController
-        if isUnderLinerNeeded {
-            underLiner = UIView.init()
-            underLiner.backgroundColor = UIColor.clear
-            addSubview(underLiner)
-            underLiner.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                underLiner.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
-                underLiner.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
-                underLiner.heightAnchor.constraint(equalToConstant: 2.0),
-                underLiner.topAnchor.constraint(equalTo: self.bottomAnchor, constant: -2.0)
-                ])
-        }
+    
+        
         let selectorWidth = frame.width / CGFloat(buttonTitles.count)
         let y = (self.frame.maxY - self.frame.minY) - 4.0
         selector = UIView.init(frame: CGRect.init(x: 0, y: y, width: selectorWidth, height: 4.0))
@@ -141,39 +131,60 @@ import UIKit
     
     
     @objc func buttonTapped(button: UIButton) {
+        
         var selectorStartPosition: CGFloat!
+        
         for (buttonIndex,btn) in buttons.enumerated() {
+            
             btn.setTitleColor(textColor, for: .normal)
+            
             if btn == button {
+                
                 selectedSegmentIndex = buttonIndex
+                
                 selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(buttonIndex)
+                
                 UIView.animate(withDuration: 0.3, animations: {
                     self.selector.frame.origin.x = selectorStartPosition
                     btn.backgroundColor = UIColor(named: GotoMarketColors.VegeCellBackground)
                 })
+                
                 btn.setTitleColor(selectorTextColor, for: .normal)
             }
         }
+        
         sendActions(for: .valueChanged)
     }
+    
+    
     func updateSegmentedControlSegs(index: Int) {
+        
         var selectorStartPosition: CGFloat!
+        
         for btn in buttons {
             btn.setTitleColor(textColor, for: .normal)
         }
+        
         selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(index)
+        
         UIView.animate(withDuration: 0.3, animations: {
             self.selector.frame.origin.x = selectorStartPosition
         })
+        
         buttons[index].setTitleColor(selectorTextColor, for: .normal)
     }
+    
     override func sendActions(for controlEvents: UIControlEvents) {
         super.sendActions(for: controlEvents)
+        
         var selectorStartPosition: CGFloat!
+        
         selectorStartPosition = frame.width / CGFloat(buttons.count) * CGFloat(selectedSegmentIndex)
+        
         UIView.animate(withDuration: 0.3, animations: {
             self.selector.frame.origin.x = selectorStartPosition
         })
+        
         buttons[selectedSegmentIndex].setTitleColor(selectorTextColor, for: .normal)
     }
 }
